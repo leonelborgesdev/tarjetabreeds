8625fe36f5facfd5b7d85bcc2e68e610af9c7bbb
leonelborgesdev <leonel03t@gmail.com>
2024-02-13 01:12:28 -0400

    form

--- src/TarjetaBreeds/TarjetaBreeds.jsx
+++ src/TarjetaBreeds/TarjetaBreeds.jsx
@@ -2,5 +2,26 @@ import React from "react";
 import "./TarjetaBreeds.css";
 
 export const TarjetaBreeds = () => {
-  return <div className="box"></div>;
+  return (
+    <div className="box">
+      <form>
+        <h2>Login</h2>
+        <div className="inputtBx">
+          <span></span>
+          <input type="text" placeholder="Username" />
+        </div>
+        <div className="inputtBx">
+          <span></span>
+          <input type="password" placeholder="Username" />
+        </div>
+        <div className="inputBx">
+          <input type="submit" value="Sing in" />
+        </div>
+        <div className="group">
+          <a href="">Forget Password</a>
+          <a href="">Singup</a>
+        </div>
+      </form>
+    </div>
+  );
 };
