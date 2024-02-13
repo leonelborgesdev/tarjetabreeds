58f2492d5713a6868c2c302a18246d5cecec0ffa
leonelborgesdev <leonel03t@gmail.com>
2024-02-13 18:34:11 -0400

    shape css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { TarjetaBreeds } from "./TarjetaBreeds/TarjetaBreeds";
+import { TarjetaPerfil } from "./TarjetaPerfil/TarjetaPerfil";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <TarjetaPerfil />
       <TarjetaBreeds />
     </div>
   );
