8245159b8538613386ee532a1cef2b2e26a11015
leonelborgesdev <leonel03t@gmail.com>
2024-02-13 18:14:41 -0400

    tarjeta perfil info

---
+++ src/TarjetaPerfil/TarjetaPerfil.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const TarjetaPerfil = () => {
+  return (
+    <div className="container">
+      <div className="shape">
+        <div className="image"></div>
+      </div>
+      <h3>Leonel Borges</h3>
+      <h4 className="title">Web Designer</h4>
+      <p>Male</p>
+      <br />
+      14-Sep-1993
+      <br />
+      @leonelborges
+    </div>
+  );
+};
