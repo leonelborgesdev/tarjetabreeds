48f9afca8171ccf66ad657ad9f8cf02a9d583b46
leonelborgesdev <leonel03t@gmail.com>
2024-02-15 23:52:15 -0400

    faceback link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { TarjetaBreeds } from "./TarjetaBreeds/TarjetaBreeds";
 import { TarjetaPerfil } from "./TarjetaPerfil/TarjetaPerfil";
 import "./App.css";
+import { TarjetaMovible } from "./TarjetaMovible/TarjetaMovible";
 
 function App() {
   return (
     <div className="App">
+      <TarjetaMovible />
       <TarjetaPerfil />
       <TarjetaBreeds />
     </div>
--- src/TarjetaMovible/TarjetaMovible.jsx
+++ src/TarjetaMovible/TarjetaMovible.jsx
@@ -5,10 +5,16 @@ export const TarjetaMovible = () => {
   return (
     <div className="card">
       <div className="face front">
-        <img />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1fJMY2RcJs4XGfAqm9GVjSxkoqCO2wr7lkg&usqp=CAU" />
         <h3>Japan</h3>
       </div>
-      <div className="face back"></div>
+      <div className="face back">
+        <h3>Japan</h3>
+        <p>Lorme</p>
+        <div className="link">
+          <a href="#">Detail</a>
+        </div>
+      </div>
     </div>
   );
 };
