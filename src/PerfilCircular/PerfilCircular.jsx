4b79669194915c988c496d9f0bf91c3eac8b3b9b
leonelborgesdev <leonel03t@gmail.com>
2024-02-22 23:05:06 -0400

    estilos containerperfilcircular

--- src/PerfilCircular/PerfilCircular.jsx
+++ src/PerfilCircular/PerfilCircular.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import "./PerfilCircular.css";
 
 export const PerfilCircular = () => {
-  return <div>PerfilCircular</div>;
+  return (
+    <div>
+      <div className="container_perfil"></div>
+    </div>
+  );
 };
