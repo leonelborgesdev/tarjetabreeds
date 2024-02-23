ffc6358d46aa8137a4df9b58f7c06fd0a2d8242d
leonelborgesdev <leonel03t@gmail.com>
2024-02-22 22:51:26 -0400

    perfilcircular

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { TarjetaBreeds } from "./TarjetaBreeds/TarjetaBreeds";
 import { TarjetaPerfil } from "./TarjetaPerfil/TarjetaPerfil";
 import "./App.css";
 import { TarjetaMovible } from "./TarjetaMovible/TarjetaMovible";
+import { PerfilCircular } from "./PerfilCircular/PerfilCircular";
 
 function App() {
   return (
     <div className="App">
+      <PerfilCircular />
       <TarjetaMovible />
       <TarjetaPerfil />
       <TarjetaBreeds />
