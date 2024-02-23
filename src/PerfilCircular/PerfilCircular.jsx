29fed7ded4d50a9bc87b1a45a8e8c5395b90dc1d
leonelborgesdev <leonel03t@gmail.com>
2024-02-22 23:47:31 -0400

    image2 perritos

--- src/PerfilCircular/PerfilCircular.jsx
+++ src/PerfilCircular/PerfilCircular.jsx
@@ -7,9 +7,15 @@ export const PerfilCircular = () => {
       <div className="container_perfil">
         <div className="content">
           <img
+            className="image_1"
             src={
               "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Brad_Pitt_2019_by_Glenn_Francis.jpg/800px-Brad_Pitt_2019_by_Glenn_Francis.jpg"
             }
+            alt=""
+          />
+          <img
+            className="image_2"
+            src="https://i.pinimg.com/originals/37/80/cf/3780cf3893cb952e27e66345bc6074b0.gif"
             alt=""
           />
         </div>
