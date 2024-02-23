d9bcb1ab76ed2d340ff5103d9255482926cb80e9
leonelborgesdev <leonel03t@gmail.com>
2024-02-22 23:29:11 -0400

    img content

--- src/PerfilCircular/PerfilCircular.jsx
+++ src/PerfilCircular/PerfilCircular.jsx
@@ -4,7 +4,16 @@ import "./PerfilCircular.css";
 export const PerfilCircular = () => {
   return (
     <div>
-      <div className="container_perfil"></div>
+      <div className="container_perfil">
+        <div className="content">
+          <img
+            src={
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Brad_Pitt_2019_by_Glenn_Francis.jpg/800px-Brad_Pitt_2019_by_Glenn_Francis.jpg"
+            }
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
